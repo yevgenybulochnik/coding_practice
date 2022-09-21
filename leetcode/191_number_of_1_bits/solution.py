e89b88a5974c6e9e8d0507dc0948d9168f3e4517
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2022-09-21 06:48:15 -0500

    191 Number of 1 Bits solution using bit shift

--- leetcode/191_number_of_1_bits/solution.py
+++ leetcode/191_number_of_1_bits/solution.py
@@ -8,4 +8,14 @@ def hammingWeight(n: int) -> int:
     return counter["1"]
 
 
+# Solution using bit shifting
+def hammingWeight2(n: int) -> int:
+    res = 0
+    while n:
+        res += n % 2
+        n = n >> 1  # this is how you shift the bit by 1
+    return res
+
+
 print(hammingWeight(0b00000000000000000000000000001011))
+print(hammingWeight2(0b00000000000000000000000000001011))
